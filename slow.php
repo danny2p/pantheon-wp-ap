e24d8bd35c6242307220eca7210cb20da1a305d6
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2025-01-20 21:11:00 -0700

    no cache headers

--- slow.php
+++ slow.php
@@ -1,5 +1,11 @@
 <?php
 
+// Set headers to prevent caching
+header("Expires: Thu, 1 Jan 1970 00:00:00 GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 function slow_call() {
   // Sleep for 10 seconds
   sleep(10);
--- wp-content/plugins/slow-page/slow-page.php
+++ wp-content/plugins/slow-page/slow-page.php
@@ -15,6 +15,9 @@ function slow_call() {
 }
 
 function slow_page_content() {
+  header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
   echo slow_call();
 }
 
