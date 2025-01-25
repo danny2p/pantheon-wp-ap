a967b0bcf2666ddd0f671c0afaf2884fcc574682
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2025-01-25 15:12:33 -0700

    simulate high-volume surge for demo

---
+++ wp-content/plugins/flood-page/flood-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: Slow Page
+ */
+
+// Register the shortcode
+add_shortcode( 'slow-page', 'slow_page_shortcode' );
+
+function slow_page_shortcode() {
+  if ( is_user_logged_in() ) {
+    ob_start();
+    ?>
+    <form method="post">
+      <label for="target_url">Target URL:</label>
+      <input type="text" name="target_url" id="target_url" value="" size="50">
+      <input type="submit" value="Flood">
+    </form>
+    <?php
+
+    if ( isset( $_POST['target_url'] ) ) {
+      $target_url = esc_url_raw( $_POST['target_url'] );
+
+      // Perform the flood asynchronously
+      for ( $i = 0; $i < 100; $i++ ) {
+        wp_remote_get( $target_url, array(
+          'timeout' => 0.1,  // Set a short timeout (in seconds)
+          'blocking' => false // Make the request non-blocking
+        ) );
+      }
+
+      echo "<p>Flood sent to $target_url</p>";
+    }
+
+    return ob_get_clean();
+  } else {
+    return "<p>You must be logged in to access this page.</p>";
+  }
+}
+?>
